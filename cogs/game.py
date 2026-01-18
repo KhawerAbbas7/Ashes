32990154e7437eec00890123bfa3d2b9f1ac0769
KhawerAbbas7 <akhawer875@gmail.com>
2026-01-18 12:41:49 +0500

    Update

--- cogs/game.py
+++ cogs/game.py
@@ -228,67 +228,67 @@ class Game():
     inn = self.currentInning
     img=Image.open(os.path.join(BASE_DIR,"templates","battingSummary.png")).convert("RGBA")
     draw=ImageDraw.Draw(img)
-    font=ImageFont.truetype(os.path.join(BASE_DIR,"fonts","Helvetica-Bold.ttf"),120)
+    font=ImageFont.truetype(os.path.join(BASE_DIR,"fonts","Helvetica-Bold.ttf"),120*0.256)
     darkVoilet = "#32267B"
     white= "#EAEDF2"
     inningNum = f"INNING {inn.inningNo}"
     battingTeam = inn.battingTeam.name.upper()
     bowlingTeam= inn.bowlingTeam.name.upper()
-    draw.text(((5000-font.getlength(inningNum))/2,554.5),inningNum,font=font,fill=white)
-    draw.text((300,554.5),battingTeam,font=font,fill=darkVoilet)
-    draw.text((3171.7,554.5),bowlingTeam,font=font,fill=darkVoilet)
-    font2=ImageFont.truetype(os.path.join(BASE_DIR,"fonts","Helvetica-Bold.ttf"),110)
-    font=ImageFont.truetype(os.path.join(BASE_DIR,"fonts","Helvetica-Bold.ttf"),150)
-    y = 1017
-    offset = 303
+    draw.text((((5000*0.256)-font.getlength(inningNum))/2,554.5*0.256),inningNum,font=font,fill=white)
+    draw.text((300*0.256,554.5*0.256),battingTeam,font=font,fill=darkVoilet)
+    draw.text((3171.7*0.256,554.5*0.256),bowlingTeam,font=font,fill=darkVoilet)
+    font2=ImageFont.truetype(os.path.join(BASE_DIR,"fonts","Helvetica-Bold.ttf"),110*0.256)
+    font=ImageFont.truetype(os.path.join(BASE_DIR,"fonts","Helvetica-Bold.ttf"),150*0.256)
+    y = 1017*0.256
+    offset = 303*0.256
     for p,i in inn.batters.items():
-      draw.text((300,y),p.name.upper()[:18],font=font,fill=darkVoilet)
+      draw.text((300*0.256,y),p.name.upper()[:18],font=font,fill=darkVoilet)
       batterRuns = f"{i.runs}"
       batterBalls = f"{i.balls}"
       l = font.getlength(batterRuns)
-      b = font2.getlength(batterBalls)+20
-      draw.text((4700-(l+b),y),batterRuns,font=font,fill=darkVoilet)
-      draw.text(((4700-b),y+30),batterBalls,font=font2,fill=darkVoilet)
+      b = font2.getlength(batterBalls)+(20*0.256)
+      draw.text(((4700*0.256)-(l+b),y),batterRuns,font=font,fill=darkVoilet)
+      draw.text((((4700*0.256)-b),y+(30*0.256)),batterBalls,font=font2,fill=darkVoilet)
       y += offset
-    font=ImageFont.truetype(os.path.join(BASE_DIR,"fonts","Helvetica-Bold.ttf"),180)
+    font=ImageFont.truetype(os.path.join(BASE_DIR,"fonts","Helvetica-Bold.ttf"),180*0.256)
     inningScore = f"{inn.runs}/{inn.wickets} ({self.ballsToOvers(inn.balls)}) {'d' if inn.declared else ''}"
-    draw.text(((5000-font.getlength(inningScore))/2,4582+54.5),inningScore,font=font,fill=darkVoilet)
-    img=img.convert("RGB").resize((1200,1200),Image.LANCZOS)
+    draw.text((((5000*0.256)-font.getlength(inningScore))/2,(4582*0.256)+(54.5*0.256)),inningScore,font=font,fill=darkVoilet)
+    
     with BytesIO() as image_binary:
       img.save(image_binary, 'PNG')
       image_binary.seek(0)
       return discord.File(fp=image_binary, filename='battingSC.png')
   def bowlingCard(self):
     inn = self.currentInning
-    img = Image.open(os.path.join(BASE_DIR,"templates","bowlingSummary.png")).convert("RGBA")
+    img = Image.open(os.path.join11(BASE_DIR,"templates","bowlingSummary.png")).convert("RGBA")
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype(os.path.join(BASE_DIR,"fonts","Helvetica-Bold.ttf"),120)
+    font = ImageFont.truetype(os.path.join(BASE_DIR,"fonts","Helvetica-Bold.ttf"),120*0.256)
     darkVoilet = "#32267B"
     white = "#EAEDF2"
     inningNum = f"INNING {inn.inningNo}"
     battingTeam = inn.battingTeam.name.upper()
     bowlingTeam = inn.bowlingTeam.name.upper()
-    draw.text(((5000-font.getlength(inningNum))/2, 554.5), inningNum, font=font, fill=white)
-    draw.text((300, 554.5), battingTeam, font=font, fill=darkVoilet)
-    draw.text((3171.7, 554.5), bowlingTeam, font=font, fill=darkVoilet)
-    y = 1225
-    offset = 303
+    draw.text((((5000*0.256)-font.getlength(inningNum))/2, 554.5*0.256), inningNum, font=font, fill=white)
+    draw.text((300*0.256, 554.5*0.256), battingTeam, font=font, fill=darkVoilet)
+    draw.text((3171.7*0.256, 554.5*0.256), bowlingTeam, font=font, fill=darkVoilet)
+    y = 1225*0.256
+    offset = 303*0.256
     for p, i in inn.bowlers.items():
       bowler = p.name.upper()[:18]
       w = str(i.wickets)
       o = str(self.ballsToOvers(i.balls))
       r = str(i.runsConceded)
       eo = str(round((i.runsConceded/i.balls)*6, 2)) if i.balls else "0.0"
-      draw.text((350, y), bowler, font=font, fill=darkVoilet)
-      draw.text((3023.2+98.4/2-font.getlength(w)/2, y), w, font=font, fill=darkVoilet)
-      draw.text((3398.1+81.9/2-font.getlength(o)/2, y), o, font=font, fill=darkVoilet)
-      draw.text((3761.9+70.3/2-font.getlength(r)/2, y), r, font=font, fill=darkVoilet)
-      draw.text((4175.2+145.5/2-font.getlength(eo)/2, y), eo, font=font, fill=darkVoilet)
+      draw.text((350*0.256, y), bowler, font=font, fill=darkVoilet)
+      draw.text(((3023.2*0.256)+(98.4*0.256)/2-font.getlength(w)/2, y), w, font=font, fill=darkVoilet)
+      draw.text(((3398.1*0.256)+(81.9*0.256)/2-font.getlength(o)/2, y), o, font=font, fill=darkVoilet)
+      draw.text(((3761.9*0.256)+(70.3*0.256)/2-font.getlength(r)/2, y), r, font=font, fill=darkVoilet)
+      draw.text(((4175.2*0.256)+(145.5*0.256)/2-font.getlength(eo)/2, y), eo, font=font, fill=darkVoilet)
       y += offset
-    font = ImageFont.truetype(os.path.join(BASE_DIR,"fonts","Helvetica-Bold.ttf"), 180)
+    font = ImageFont.truetype(os.path.join(BASE_DIR,"fonts","Helvetica-Bold.ttf"), 180*0.256)
     inningScore = f"{inn.runs}/{inn.wickets}({self.ballsToOvers(inn.balls)}) {'d' if inn.declared else ''}"
-    draw.text(((5000-font.getlength(inningScore))/2, 4582+54.5), inningScore, font=font, fill=darkVoilet)
-    img=img.convert("RGB").resize((1200,1200),Image.LANCZOS)
+    draw.text((((5000*0.256)-font.getlength(inningScore))/2, (4582*0.256)+(54.5*0.256)), inningScore, font=font, fill=darkVoilet)
+    
     with BytesIO() as image_binary:
       img.save(image_binary, 'PNG')
       image_binary.seek(0)
@@ -296,43 +296,44 @@ class Game():
   def matchSummaryCard(self):
     img = Image.open(os.path.join(BASE_DIR, "templates","matchSummary.png")).convert("RGBA")
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype(os.path.join(BASE_DIR,"fonts","Helvetica-Bold.ttf"),120)
+  
+    font = ImageFont.truetype(os.path.join(BASE_DIR,"fonts","Helvetica-Bold.ttf"),120*0.256)
     darkVoilet = "#32267B"
     white = "#EAEDF2"
-    y = 588.4
-    offset = 966.2 
+    y = 588.4*0.256
+    offset = 966.2*0.256
     for inn in self.innings:
       battingTeam = inn.battingTeam.name.upper() 
       if inn.inningNo == 3 and self.followOnTeam:
         battingTeam += " (f/o)"
       score = f"{inn.runs}/{inn.wickets} ({self.ballsToOvers(inn.balls)}) {'d' if inn.declared else ''}"
-      draw.text((300, y + 54.5), battingTeam, font=font, fill=white)
-      draw.text((4700 - font.getlength(score), y + 54.5), score, font=font, fill=white)
+      draw.text((300*0.256, y + (54.5*0.256)), battingTeam, font=font, fill=white)
+      draw.text(((4700*0.256) - font.getlength(score), y + (54.5*0.256)), score, font=font, fill=white)
       topBat = sorted(inn.batters.items(), key=lambda x: x[1].runs, reverse=True)[:2]
       topBowl = sorted(inn.bowlers.items(), key=lambda x: x[1].wickets, reverse=True)[:2]
-      y2 = y + 262
-      offset2 = 303
+      y2 = y + (262*0.256)
+      offset2 = 303*0.256
       for k in range(2):
         if k < len(topBat):
           p, i = topBat[k]
           if i.balls > 0:
             batter = p.name.upper()[:15]
             batterScore = f"{i.runs} ({i.balls})"
-            draw.text((320, y2 + 70.5), batter, font=font, fill=darkVoilet)
-            draw.text((2180 - font.getlength(batterScore), y2 + 70.5), batterScore, font=font, fill=darkVoilet)
+            draw.text((320*0.256, y2 + (70.5*0.256)), batter, font=font, fill=darkVoilet)
+            draw.text(((2180*0.256) - font.getlength(batterScore), y2 + (70.5*0.256)), batterScore, font=font, fill=darkVoilet)
         if k < len(topBowl):
           p, i = topBowl[k]
           if i.balls > 0:
             bowler = p.name.upper()[:15]
             bowlerScore = f"{i.wickets}/{i.runsConceded} ({self.ballsToOvers(i.balls)})"
-            draw.text((2820, y2 + 70.5), bowler, font=font, fill=darkVoilet)
-            draw.text((4680 - font.getlength(bowlerScore), y2 + 70.5), bowlerScore, font=font, fill=darkVoilet)
+            draw.text((2820*0.256, y2 + (70.5*0.256)), bowler, font=font, fill=darkVoilet)
+            draw.text(((4680*0.256) - font.getlength(bowlerScore), y2 + (70.5*0.256)), bowlerScore, font=font, fill=darkVoilet)
         y2 += offset2
       y += offset
     footer = self.matchStatus().upper()
-    font=ImageFont.truetype(os.path.join(BASE_DIR,"fonts","Helvetica-Bold.ttf"),120)
-    draw.text(((5000-font.getlength(footer))/2,4582+79.5),footer,font=font,fill=darkVoilet)
-    img=img.convert("RGB").resize((1200,1200),Image.LANCZOS)
+    font=ImageFont.truetype(os.path.join(BASE_DIR,"fonts","Helvetica-Bold.ttf"),120*0.256)
+    draw.text((((5000*0.256)-font.getlength(footer))/2,(4582*0.256)+79.5),footer,font=font,fill=darkVoilet)
+    
     with BytesIO() as image_binary:
       img.save(image_binary, 'PNG')
       image_binary.seek(0)

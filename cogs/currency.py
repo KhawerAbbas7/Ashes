8d12a3f8f918e9d83910ab27e3b167599b8ce516
KhawerAbbas7 <akhawer875@gmail.com>
2026-07-12 12:47:44 +0500

    Add claim and cooldowns command

--- cogs/currency.py
+++ cogs/currency.py
@@ -3,6 +3,8 @@ from discord import Embed, Colour
 from discord import ui 
 from discord.ext import commands, tasks
 from cogs.views import *
+import random 
+from datetime import datetime, timezone, timedelta
 from discord.ext.commands.cooldowns import BucketType
 class K_Converter(commands.Converter):
   async def convert(self, ctx, argument):
@@ -113,6 +115,24 @@ class Currency(commands.Cog, name= "Currency"):
       container.add_item(ui.TextDisplay(f"{target.mention} bro you won at life!! {ctx.author} have decided to give you {amount:,}."))
       view.add_item(container)
       await ctx.send(view= view)
+  @commands.command(aliases=['cd'], description='View all your current cooldowns.')
+  async def cooldowns(self, ctx):
+    rows = await ctx.bot.cfetchall("SELECT command, expiresAt FROM cooldowns WHERE userId = ?", (ctx.author.id,))
+    user_cds = {r[0]: r[1] for r in rows}
+    now = time.time()
+    texts = []
+    for cmd in ['daily', 'weekly', 'monthly', 'vote', 'claim']:
+      exp = user_cds.get(cmd, 0)
+      if cmd == 'vote' and exp < now:
+        texts.append(f"**{cmd.title()}**: <t:{exp}:R>" if exp > now else f"**{cmd.title()}**: Ready! -> [Vote](https://top.gg/bot/1443165621100740668/vote)")
+      else:
+        texts.append(f"**{cmd.title()}**: <t:{exp}:R>" if exp > now else f"**{cmd.title()}**: Ready!")
+    view = ui.LayoutView(timeout=30)
+    container = ui.Container(accent_color=Colour.from_str("#56804c"))
+    container.add_item(ui.TextDisplay(f"### {ctx.author.name}'s Cooldowns\n" + "\n".join(texts)))
+    view.add_item(container)
+    await ctx.send(view=view)
+    
   @commands.command(aliases= ['bal'], description= 'View the balance of yourself or others.')
   async def wallet(self, ctx, target: discord.User = None):
     if not target: target=ctx.author
@@ -121,6 +141,34 @@ class Currency(commands.Cog, name= "Currency"):
     view=ui.LayoutView(timeout=30)
     container=ui.Container(accent_color=Colour.from_str("#56804c"))
     container.add_item(ui.TextDisplay(f"### {target.name}'s Coins\n{bal}"))
+    view.add_item(container)
+    await ctx.send(view= view)
+  @commands.command(aliases= [], description= 'Claim the money after every hour but only in Official server and if you have played today.')
+  async def claim(self, ctx):
+    if not ctx.guild or ctx.guild.id != 1459434908932902914:
+      return await ctx.send(embed= Embed(title='Command can\'t be run', description=f'This command can only be run in [Official Server]({ctx.bot.supportServerLink}).', color=Colour.from_str('#b30707')))
+    pkt = timezone(timedelta(hours=5))
+    now = datetime.now(pkt)
+    today_start = int(now.replace(hour=0, minute=0, second=0, microsecond=0).timestamp()) 
+    q = "SELECT EXISTS(SELECT 1 FROM deliveries WHERE (batterId = ? OR bowlerId = ?) AND timestamp >= ?)"
+    row = await ctx.bot.fetchrow(q, (ctx.author.id, ctx.author.id, today_start))
+    c = row[0] if row else False
+    if not c:
+      return await ctx.send(embed= Embed(title='Command can\'t be run', description=f'To be able to run this command you have to play at least one game today (Starting the day from 12 AM PKT).', color=Colour.from_str('#b30707')))
+    cooldown = await ctx.bot.cfetchrow("SELECT expiresAt,lastClaimAt FROM cooldowns WHERE userId = ? AND command = ?", (ctx.author.id, 'claim'))
+    streak = 1
+    if cooldown and cooldown[0] >= time.time():
+      return await ctx.send(embed= Embed(title='Command on cooldown', description=f'The command is on cooldown, please try again later at <t:{cooldown[0]}:F>.', color=Colour.from_str('#b30707')))
+    reward = random.randint(100, 3000)
+    claimTime = int(time.time())
+    expires = claimTime+ 3600
+    await ctx.bot.cexecute("INSERT INTO cooldowns (userId, command, lastClaimAt, expiresAt) VALUES (?,?,?,?) ON CONFLICT(userId, command) DO UPDATE SET lastClaimAt=excluded.lastClaimAt, expiresAt = excluded.expiresAt", (ctx.author.id, 'claim', claimTime, expires))
+    bal = await ctx.bot.cfetchrow("SELECT coins FROM users WHERE userId = ?", (ctx.author.id,))
+    bal = bal[0] if bal else 0
+    await ctx.bot.cexecute("INSERT INTO users (userId, coins) VALUES (?,?) ON CONFLICT(userId) DO UPDATE SET coins =excluded.coins + coins", (ctx.author.id, reward))
+    view=ui.LayoutView(timeout=30)
+    container=ui.Container(accent_color=Colour.from_str("#56804c"))
+    container.add_item(ui.TextDisplay(f"### {ctx.author.name} Claimed {reward}\n{bal} + {reward} = {bal + reward}"))
     view.add_item(container)
     await ctx.send(view= view)
   @commands.command(aliases= ['d'], description= 'Claim the daily money.')

26068f7775571d4e403fc3546390d9774f4f0a6f
KhawerAbbas7 <akhawer875@gmail.com>
2026-08-06 13:49:24 +0500

    Update Owner IDs
    Add Khawi's new account as a owner.

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ async def get_pre(bot, message):
     return commands.when_mentioned_or(*prefix_return)(bot, message)
 class Ashes(commands.Bot):
   def __init__(self, intents= discord.Intents.all(), command_prefix= get_pre,case_insensitive=True, strip_after_prefix= True):
-    super().__init__(intents=intents, command_prefix= get_pre,case_insensitive=True, strip_after_prefix= True,help_command=None, owner_ids= {759713678013890560})
+    super().__init__(intents=intents, command_prefix= get_pre,case_insensitive=True, strip_after_prefix= True,help_command=None, owner_ids= {759713678013890560,1534810674281189376}) #92.97 & durefishansaleem
     self.supportServerLink = "https://discord.gg/uxchR7sKd2"
     self.creationBlocked= False
     self.games = {}
